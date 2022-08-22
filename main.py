7fef5827796b9a81cd658acde4aa30c1aa132478
taomu010 <111723200+taomu010@users.noreply.github.com>
2022-08-22 16:47:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
+def get_today():
+  today =datetime.date.today()
+  return today
+
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
@@ -57,6 +61,6 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"date":{"value":date},"city":{"value":city},"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"today":{"value":today, "color":get_random_color()},"weather":{"value":wea, "color":get_random_color()},"temperature":{"value":temperature, "color":get_random_color()},"love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
